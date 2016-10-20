c05c1a7793d247fec64e6eb5094bdb4b127e38ab
Octav <tavy315@users.noreply.github.com>
2016-10-20 16:31:50 +0300

    Update CleanupPlugin.php
    Added logging of the files excluded

--- src/CleanupPlugin.php
+++ src/CleanupPlugin.php
@@ -120,27 +120,29 @@ class CleanupPlugin implements PluginInterface, EventSubscriberInterface
         $packageName = $package->getPrettyName();
         $packageDir = $targetDir ? $packageName . '/' . $targetDir : $packageName;
 
-        $rules = isset($this->rules[$packageName]) ? $this->rules[$packageName] : null;
-        if (!$rules) {
+        if (!isset($this->rules[$packageName])) {
             return;
         }
+
+        $rules = (array) $this->rules[$packageName];
 
         $dir = $this->filesystem->normalizePath(realpath($vendorDir . '/' . $packageDir));
         if (!is_dir($dir)) {
             return false;
         }
 
-        foreach ((array) $rules as $part) {
+        foreach ($rules as $part) {
             // Split patterns for single globs (should be max 260 chars)
             $patterns = explode(' ', trim($part));
 
             foreach ($patterns as $pattern) {
                 try {
                     foreach (glob($dir . '/' . $pattern) as $file) {
+                        $this->io->write("Excluding {$file}");
                         $this->filesystem->remove($file);
                     }
                 } catch (\Exception $e) {
-                    $this->io->write("Could not parse $packageDir ($pattern): " . $e->getMessage());
+                    $this->io->writeError("Could not parse $packageDir ($pattern): " . $e->getMessage());
                 }
             }
         }
